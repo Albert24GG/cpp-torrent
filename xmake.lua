0ad8380728a83b350ca0d88df3375c1aea5219c0
Albert24GG <guiman.albert09@gmail.com>
2024-07-07 21:59:51 +0300

    [proj]: Give up on c++20 modules for lack of clangd support

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,7 @@
 add_rules("mode.debug", "mode.release")
 
 set_languages("c++23")
-set_policy("build.c++.modules", true)
 
 target("test")
     set_kind("binary")
-    add_files("src/*.cpp", "src/*.mpp")
+    add_files("src/*.cpp")

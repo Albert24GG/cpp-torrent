24a7e1d50bf79bf2bfbf578d477389698b212e5e
Albert24GG <guiman.albert09@gmail.com>
2024-07-21 23:00:37 +0300

    [refactor]: Rename `offset_start` to `start_off` and add getters
    Signed-off-by: Albert24GG <guiman.albert09@gmail.com>

--- src/File.hpp
+++ src/File.hpp
@@ -5,8 +5,8 @@
 namespace torrent::fs {
 class File {
     public:
-        File(std::filesystem::path path, size_t offset_start, size_t length)
-            : path(std::move(path)), offset_start(offset_start), length(length) {}
+        File(std::filesystem::path path, size_t start_off, size_t length)
+            : path(std::move(path)), start_off(start_off), length(length) {}
 
         File()                       = default;
         File(const File&)            = default;
@@ -15,9 +15,13 @@ class File {
         File& operator=(File&&)      = default;
         ~File()                      = default;
 
+        [[nodiscard]] const std::filesystem::path& get_path() const { return path; }
+        [[nodiscard]] size_t                       get_start_off() const { return start_off; }
+        [[nodiscard]] size_t                       get_length() const { return length; }
+
     private:
         std::filesystem::path path;
-        size_t                offset_start{};
+        size_t                start_off{};
         size_t                length{};
 };
 }  // namespace torrent::fs

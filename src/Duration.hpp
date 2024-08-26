15975b66e0e50e712f4025df7cfcce1dde10eb75
Albert24GG <guiman.albert09@gmail.com>
2024-08-26 23:16:51 +0300

    [feat]: Tweak some durations

--- src/Duration.hpp
+++ src/Duration.hpp
@@ -7,9 +7,9 @@ namespace torrent::duration {
 static constexpr std::chrono::seconds      HANDSHAKE_TIMEOUT{10};
 static constexpr std::chrono::seconds      CONNECTION_TIMEOUT{10};
 static constexpr std::chrono::seconds      SEND_MSG_TIMEOUT{10};
-static constexpr std::chrono::seconds      RECEIVE_MSG_TIMEOUT{10};
-static constexpr std::chrono::seconds      REQUEST_TIMEOUT{10};
+static constexpr std::chrono::seconds      RECEIVE_MSG_TIMEOUT{20};
+static constexpr std::chrono::seconds      REQUEST_TIMEOUT{5};
 static constexpr std::chrono::seconds      PEER_CLEANUP_INTERVAL{10};
-static constexpr std::chrono::milliseconds REQUEST_INTERVAL{500};
+static constexpr std::chrono::milliseconds REQUEST_INTERVAL{100};
 
 }  // namespace torrent::duration

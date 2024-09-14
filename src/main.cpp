ff9387b8abd00ffb8b97f9138f34db24c6c863ee
Albert24GG <guiman.albert09@gmail.com>
2024-09-14 14:08:37 +0300

    [feat]: Add cli interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,57 @@
-#include <iostream>
+#include "Logger.hpp"
+#include "ProgressBar.hpp"
+#include "TorrentClient.hpp"
 
-#include "Bencode.hpp"
+#include <argparse/argparse.hpp>
 
 int main(int argc, char** argv) {
-    std::string test{"l4:spami4s2ee"};
+    argparse::ArgumentParser arg_parser("cpp-torrent");
 
-    Bencode::BencodeList res = std::get<Bencode::BencodeList>(Bencode::BDecode(test));
+    arg_parser.add_argument("torrent_file").help("Path to the .torrent file");
 
-    std::cout << std::get<Bencode::BencodeInt>(res[1]);
+    arg_parser.add_argument("-o", "--output-dir")
+        .help("Output directory")
+        .default_value(std::string("."));
+
+    arg_parser.add_argument("-l", "--logging")
+        .help("Enable logging")
+        .default_value(false)
+        .implicit_value(true);
+
+    arg_parser.add_argument("-lf", "--log-file")
+        .help("Path to the log file")
+        .default_value(std::string("./log.txt"));
+
+    try {
+        arg_parser.parse_args(argc, argv);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << arg_parser;
+        return 1;
+    }
+
+    if (arg_parser.get<bool>("--logging")) {
+        torrent::logger::init(arg_parser.get<std::string>("--log-file"));
+#ifdef DEBUG
+        torrent::logger::set_level(torrent::logger::Level::debug);
+#else
+        torrent::logger::set_level(torrent::logger::Level::info);
+#endif
+    } else {
+        torrent::logger::set_level(torrent::logger::Level::off);
+    }
+
+    torrent::TorrentClient client(
+        arg_parser.get<std::string>("torrent_file"), arg_parser.get<std::string>("--output-dir")
+    );
+
+    torrent::ui::ProgressBar progress_bar(client);
+
+    std::jthread draw_thread([&progress_bar] { progress_bar.start_draw(); });
+
+    client.start_download();
+
+    progress_bar.stop_draw();
 
     return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -2,12 +2,15 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c++23")
 
-add_requires("catch2", "cpptrace", "openssl3", "cpr", "cpp-httplib", "spdlog", "asio", "indicators")
+add_requires("catch2", "cpptrace", "openssl3", "cpr", "cpp-httplib", "spdlog", "asio", "indicators", "argparse")
 
-target("test")
+target("cpp-torrent")
+    if is_mode("debug") then
+        add_defines("DEBUG")
+    end
     set_kind("binary")
     add_files("src/*.cpp")
-    add_packages("cpptrace", "openssl3", "cpr", "spdlog", "asio", "indicators")
+    add_packages("cpptrace", "openssl3", "cpr", "spdlog", "asio", "indicators", "argparse")
 
 rule("run_tests")
     after_build(function (target)

975b95f4a27ba0ff9df1154d7b53ce5cc3d843b2
Albert24GG <guiman.albert09@gmail.com>
2024-09-25 11:45:30 +0300

    [proj]: Copy target bin to `bin/<mode>` after build

--- xmake.lua
+++ xmake.lua
@@ -4,6 +4,14 @@ set_languages("c++23")
 
 add_requires("catch2", "cpptrace", "openssl3", "cpr", "cpp-httplib", "spdlog", "asio", "indicators", "argparse")
 
+rule("copy_bin")
+    after_build(function (target)
+        build_path = "$(buildir)/$(os)/$(arch)/$(mode)/"
+        bin_path = "bin/$(mode)/"    
+        os.mkdir(bin_path)    
+        os.cp(build_path .. target:name(), bin_path)
+    end)
+
 target("cpp-torrent")
     if is_mode("debug") then
         add_defines("DEBUG")
@@ -11,6 +19,7 @@ target("cpp-torrent")
     set_kind("binary")
     add_files("src/*.cpp")
     add_packages("cpptrace", "openssl3", "cpr", "spdlog", "asio", "indicators", "argparse")
+    add_rules("copy_bin")
 
 rule("run_tests")
     after_build(function (target)

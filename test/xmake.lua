59131abea17e9dd5d62d5c1ac3b5cabd98a8302c
Albert24GG <79106176+Albert24GG@users.noreply.github.com>
2024-09-25 21:00:22 +0300

    Compile common sources for unit tests in a static lib

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,17 +1,26 @@
 add_requires("catch2", "cpp-httplib")
 
-for _, file in ipairs(os.files("*_test.cpp")) do
-     local name = path.basename(file)
-     target(name)
-         set_kind("binary")
-         set_default(false)
-         add_files(name .. ".cpp")
-         if name == "Tracker_test" then
-             add_files("MockUdpTracker.cpp", "MockHttpTracker.cpp")
-         end
+-- Create a static library for common source files
+target("common_lib")
+    set_kind("static")
+    add_files("../src/*.cpp|main.cpp|ProgressBar.cpp")
+    add_includedirs("../src")
+    add_packages("cpptrace","openssl3", "cpr", "spdlog", "asio")
 
-         add_files("../src/*.cpp|main.cpp|ProgressBar.cpp")
-         add_includedirs("../src")
-         add_packages("catch2","cpptrace", "openssl3", "cpr", "cpp-httplib", "spdlog", "asio")
-         add_tests("default")
+-- Create test targets
+for _, file in ipairs(os.files("*_test.cpp")) do
+    local name = path.basename(file)
+    target(name)
+        set_kind("binary")
+        set_default(false)
+        add_files(name .. ".cpp")
+        if name == "Tracker_test" then
+            add_packages("cpp-httplib")
+            add_files("MockUdpTracker.cpp", "MockHttpTracker.cpp")
+        end
+
+        add_includedirs("../src")
+        add_packages("catch2", "asio")
+        add_deps("common_lib")
+        add_tests("default")
 end

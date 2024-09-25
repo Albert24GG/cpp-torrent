87fe3f8d7c1cbea1eb27519b1c375cd7ce4213b8
Albert24GG <79106176+Albert24GG@users.noreply.github.com>
2024-09-25 22:06:15 +0300

    Add working github actions with ubuntu-24.04 (#1)
    * Update workflow to include 2 configs for ubuntu
    
    * Update xmake-multi-platform.yml
    
    * Replace double quotes with single quotes
    
    * Remove invalid matrix entry
    
    * Start ssh debugging before xmake setup
    
    * Use ssh debugging for all configs
    
    * Use gcc-14 and action-tmate
    
    * Run workflow only on ubuntu with gcc
    
    * Add ssh after build is done
    
    * Compile common sources for unit tests in a static lib
    
    * Use a static lib for all shared sources
    
    * Remove ssh from workflow

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,22 @@
 add_rules("mode.debug", "mode.release")
 
-includes("test")
-
 set_languages("c++23")
 
-add_requires("cpptrace", "openssl3", "cpr", "spdlog", "asio", "indicators", "argparse")
+local packages = { 
+    "cpptrace", 
+    "openssl3",
+    "cpr",
+    "spdlog",
+    "asio",
+    "indicators",
+    "argparse",
+    "catch2",
+    "cpp-httplib"
+}
+
+for _, pkg in ipairs(packages) do
+    add_requires(pkg)
+end
 
 rule("copy_bin")
     after_build(function (target)
@@ -14,11 +26,43 @@ rule("copy_bin")
         os.cp(build_path .. target:name(), bin_path)
     end)
 
+rule("config_pkgs")
+    on_load(function(target)
+        for _, pkg in ipairs(packages) do
+           target:add("packages", pkg) 
+        end
+    end)
+
+-- Create a static library for common source files
+target("common_lib")
+    add_rules("config_pkgs")
+    set_kind("static")
+    add_files("src/*.cpp|main.cpp|ProgressBar.cpp")
+    add_includedirs("src")
+
+-- Add the main target
 target("cpp-torrent")
     if is_mode("debug") then
         add_defines("DEBUG")
     end
     set_kind("binary")
-    add_files("src/*.cpp")
-    add_packages("cpptrace", "openssl3", "cpr", "spdlog", "asio", "indicators", "argparse")
-    add_rules("copy_bin")
+    add_deps("common_lib")
+    add_files("src/main.cpp","src/ProgressBar.cpp")
+    add_rules("copy_bin", "config_pkgs")
+
+-- Add the unit test targets
+for _, file in ipairs(os.files("test/*_test.cpp")) do
+    local name = path.basename(file)
+    target(name)
+        set_kind("binary")
+        set_default(false)
+        add_files("test/" .. name .. ".cpp")
+        if name == "Tracker_test" then
+            add_files("test/MockUdpTracker.cpp", "test/MockHttpTracker.cpp")
+        end
+
+        add_includedirs("src")
+        add_deps("common_lib")
+        add_rules("config_pkgs")
+        add_tests("default")
+end
